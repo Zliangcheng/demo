c46a8cb37f6e53dca817542035d6d73333fe8303
liangcheng.zhang <liangcheng.zhang@thoughtworks.com>
2022-11-29 17:42:03 +0800

    add index html

--- src/main/java/com/zliangcheng/firstdemo/access/HelloController.java
+++ src/main/java/com/zliangcheng/firstdemo/access/HelloController.java
@@ -2,9 +2,15 @@ package com.zliangcheng.firstdemo.access;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HelloController {
+
+    @GetMapping("/")
+    public ModelAndView getIndex() {
+        return new ModelAndView("index");
+    }
 
     @GetMapping("/hello")
     public String printHello() {
